eefe7b011728ef53ed3c1f136f12fd7d89b3456d
zhanghr <429763991@qq.com>
2016-05-31 14:46:59 +0800

    [add] shopcontroller；

---
+++ src/main/java/com/controller/ShopController.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zyx.entity.shop.Goods;
+import com.zyx.rpc.shop.ShopService;
+
+@RestController
+@RequestMapping("/shop")
+public class ShopController {
+	@Autowired
+	private ShopService shopService;
+	
+	  @RequestMapping(value = "/view/{id}")
+	    public Goods view(@PathVariable Long id) {
+		     Goods goods =  shopService.getGoodsbyKey(id);
+	        return goods;
+	    }
+
+}
