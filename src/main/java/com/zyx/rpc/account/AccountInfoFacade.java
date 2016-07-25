66ee008721b432573bb04649f0fb7ad9f473ffd2
sgWeiMinSheng <449598723@qq.com>
2016-07-25 18:41:24 +0800

    [modify]用户编辑接口修改

--- src/main/java/com/zyx/rpc/account/AccountInfoFacade.java
+++ src/main/java/com/zyx/rpc/account/AccountInfoFacade.java
@@ -1,15 +1,19 @@
 package com.zyx.rpc.account;
 
+import com.zyx.entity.account.param.AccountInfoParam;
+
 import java.util.Map;
 
 /**
- * Created by skmbg on 2016/6/17.
+ * Created by wms on 2016/6/17.
  *
  * @author WeiMinSheng
  * @version V1.0
  *          Copyright (c)2016 tyj-版权所有
- * @title com.zyx.rpc.account
+ * @title AccountInfoFacade.java
  */
 public interface AccountInfoFacade {
     Map<String, Object> queryAccountInfo(String token, int userId);
+
+    Map<String, Object> editAccountInfo(String token, int userId, AccountInfoParam param);
 }
